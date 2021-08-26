c782707a6967e61db7ee77ed73bdeb6a9a6f4a6e
wei.hu <wei.hu@enflame-tech.com>
2021-08-25 23:39:42 -0400

    add import syntax test

--- src/Cone/Parser/Parser.hs
+++ src/Cone/Parser/Parser.hs
@@ -12,18 +12,23 @@ import Data.List.Split
 type Parser a = P.ParsecT [L.Token] () Identity a
 
 token :: (L.Tok -> Bool) -> (L.Tok -> a) -> Parser a
-token test getVal = P.tokenPrim show nextPos testTok
+token test getVal = P.tokenPrim showTok nextPos testTok
   where nextPos :: P.SourcePos -> L.Token -> [L.Token] -> P.SourcePos
         nextPos pos _ ((L.AlexPn _ l c, _) : _)
           = P.setSourceColumn (P.setSourceLine pos l) c
         nextPos pos _ [] = pos
         testTok (_, t) = if (test t) then Just (getVal t) else Nothing
+        showTok (L.AlexPn _ l c, t) = show t
 
 keyword :: L.Tok -> Parser L.Tok
 keyword t = token (== t) (\ _ -> t)
 
+symbol = keyword
+
 kmodule = keyword L.Module
-semi = keyword L.Semi
+kimport = keyword L.Import
+
+semi = symbol L.Semi
 
 ident
   = token
@@ -38,9 +43,18 @@ getPos
        return $
          A.Location (P.sourceName pos) (P.sourceLine pos) (P.sourceColumn pos)
 
+sep = P.many1 semi
+
+namePath :: String -> [String]
+namePath = splitOn "\\"
+
+imports :: Parser [A.ImportStmt]
+imports = P.many $ f <$ kimport <*> ident <*> getPos <* sep
+  where f n pos = A.ImportStmt (namePath n) Nothing [] pos
+
 m :: Parser A.Module
-m = f <$ kmodule <*> ident <*> getPos <* (P.many semi) <* P.eof
-  where f n pos = A.Module (splitOn "\\" n) [] [] [] [] pos
+m = f <$ kmodule <*> ident <*> getPos <* sep <*> imports <* P.eof
+  where f n pos ims = A.Module (namePath n) [] [] ims [] pos
 
 parse :: String -> String -> Either P.ParseError A.Module
 parse fn input
--- test/Parser/ParserSpec.hs
+++ test/Parser/ParserSpec.hs
@@ -10,6 +10,14 @@ moduleSpec = hspec $ do
   describe "module" $ do
     it "empty module" $ do
        ((parse "" "module foo\\bar")^._Right.moduleName) `shouldBe` ["foo", "bar"]
+    
+    it "module with one import" $ do
+       ((parse "" "module foo\\bar; import foo")
+          ^._Right.imports ^? ix 0 ^._Just.importPath) `shouldBe` ["foo"]
+
+    it "module with two imports" $ do
+       ((parse "" "module foo\\bar; import foo\nimport bar")
+          ^._Right.imports ^? ix 1 ^._Just.importPath) `shouldBe` ["bar"]
 
 parserSpec = do
     moduleSpec
