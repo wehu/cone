f1b622704f55fdfe463ab19cdd7020c78af2ddee
wei.hu <wei.hu@enflame-tech.com>
2021-08-25 23:50:24 -0400

    add import syntax test

--- test/Parser/ParserSpec.hs
+++ test/Parser/ParserSpec.hs
@@ -1,22 +1,38 @@
+{-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
+
 module Parser.ParserSpec where
 
 import Test.Hspec
 import Cone.Parser.Parser
 import Cone.Parser.AST
 import Data.Either
+import Data.Text
 import Control.Lens
+import NeatInterpolation (text)
 
 moduleSpec = hspec $ do
   describe "module syntax" $ do
     it "empty module" $ do
-       ((parse "" "module foo\\bar")^._Right.moduleName) `shouldBe` ["foo", "bar"]
+       let source = unpack [text|
+           module foo\bar
+       |]
+       ((parse "" source)^._Right.moduleName) `shouldBe` ["foo", "bar"]
     
     it "module with one import" $ do
-       ((parse "" "module foo\\bar; import foo")
+       let source = unpack [text|
+           module foo\bar
+	   import foo
+       |]
+       ((parse "" source)
           ^._Right.imports ^? ix 0 ^._Just.importPath) `shouldBe` ["foo"]
 
     it "module with two imports" $ do
-       ((parse "" "module foo\\bar; import foo\nimport bar")
+       let source = unpack [text|
+           module foo\bar;
+	   import foo
+	   import bar;
+       |]
+       ((parse "" source)
           ^._Right.imports ^? ix 1 ^._Just.importPath) `shouldBe` ["bar"]
 
 parserSpec = do
