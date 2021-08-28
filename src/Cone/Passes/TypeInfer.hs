9b4ce9d70679e260fba00684a65116e7dc2c808d
wei.hu <wei.hu@enflame-tech.com>
2021-08-28 00:20:42 -0400

    add type inference

--- src/Cone/Passes/TypeInfer.hs
+++ src/Cone/Passes/TypeInfer.hs
@@ -39,8 +39,11 @@ inferTypeDef m = do
   env <- get @Env
   newTypes <- ts env
   put $ set types newTypes env
+  newTcons <- tcons env
+  put $ set funcs newTcons env
   return m
   where tdefs = universeOn (topStmts.traverse._TDef) m
+
         ts env = foldM insertType (env ^. types) tdefs
         insertType ts (BoundTypeDef (B _ t)) =
            let tn = t ^. typeName
@@ -55,7 +58,23 @@ inferTypeDef m = do
             in if args == [] then star
                 else KFunc (fmap (\(_, kk) -> case kk of
                                      Nothing -> star
-                                     Just kkk -> kkk) args) star loc 
+                                     Just kkk -> kkk) args) star loc
+        
+        tcons env = foldM insertTcon (env ^. funcs) tdefs
+        insertTcon fs (BoundTypeDef (B _ t)) = 
+           let cons = t ^. typeCons
+               f = \fs c -> do
+                 let cn = c ^. typeConName
+                  in if M.member cn fs
+                      then throwError $ "type construct has conflict name: " ++ cn
+                      else return $ M.insert cn (getTconType c t) fs
+            in foldM f fs cons
+        insertTcon fs _ = return fs
+        getTconType c t = 
+          let targs = c ^. typeConArgs
+              tn = t ^. typeName
+              pos = c ^. typeConLoc
+           in TFunc targs Nothing (TApp (s2n tn) targs pos) pos
 
 infer :: Module -> Either String (Env, Module)
 infer m = run . runError . runState initialEnv $ 
