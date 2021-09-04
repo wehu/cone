801e98fcf632e635e2b536767e4d8e109ac1c9ce
wei.hu <wei.hu@enflame-tech.com>
2021-09-04 05:43:45 -0400

    add python backend

--- src/Cone/TypeChecker/Expr.hs
+++ src/Cone/TypeChecker/Expr.hs
@@ -366,12 +366,12 @@ inferExprEffType EHandle {..} = underScope $ do
   -- check intefaces
   effName <- if not $ isn't _EffApp _ehandleEff then return $ _ehandleEff ^.effAppName
              else if not $ isn't _EffVar _ehandleEff then return $ name2String $ _ehandleEff ^.effVarName
-                  else throwError $ "expected a eff variable or application, but got " ++ ppr _ehandleEff ++ ppr _eloc
+                  else throwError $ "expected an eff variable or application, but got " ++ ppr _ehandleEff ++ ppr _eloc
   intfs <- getEnv $ effIntfs . at effName
   case intfs of
     Just is -> do let intfNames = map (\e -> e ^.funcName) _ehandleBindings 
                   if L.sort is == L.sort intfNames then return ()
-                  else throwError $ "intefaces mismatch: " ++ ppr is ++ " vs " ++ ppr intfNames
+                  else throwError $ "eff interfaces mismatch: " ++ ppr is ++ " vs " ++ ppr intfNames
     Nothing -> do
       throwError $ "cannot find effect: " ++ ppr _ehandleEff ++ ppr _eloc
   removeEff et _ehandleEff
