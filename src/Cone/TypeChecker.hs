11752426efaf126b1861e97485196348ac1a9275
wei.hu <wei.hu@enflame-tech.com>
2021-09-04 09:04:36 -0400

    add python backend

--- src/Cone/TypeChecker.hs
+++ src/Cone/TypeChecker.hs
@@ -1,12 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE ParallelListComp #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeOperators #-}
 
 module Cone.TypeChecker (Env (..), types, funcs, effs, initialEnv, initModule, checkType) where
 
