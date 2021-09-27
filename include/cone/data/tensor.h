d72b9be16e25be358c911770b4711859e3c0cb2a
wei.hu <wei.hu@enflame-tech.com>
2021-09-27 08:24:03 -0400

    add dynamic shape for tensor

--- include/cone/data/tensor.h
+++ include/cone/data/tensor.h
@@ -8,7 +8,13 @@ namespace cone {
       const std::function<object_t(const cont_t &, stack_t, effects_t, const object_t &, const object_t &)> cone__full = 
       [=](const cont_t &k, stack_t stack, effects_t effs, const object_t &elem, const object_t &dyn_dims) {
         auto typeargs = ____to_py_object(stack->back()[____typeargs]);
-        auto shape = py::cast<py::list>(typeargs)[0];
+        auto shape = py::cast<py::list>(py::cast<py::list>(typeargs)[0]);
+        auto rank = py::len(shape);
+        for (unsigned i=0; i<rank; ++i) {
+          if (py::cast<py::int_>(shape[i]) == -1) {
+            throw ____cone_exception("unsupported dynamic shape so far");
+          }
+        }
         return k(py::object(py::module_::import("numpy").attr("full")(shape, ____to_py_object(elem))));
       };
   
