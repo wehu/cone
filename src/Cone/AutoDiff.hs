770d0e58462adca562b189df5f0f04aafbe77526
wei.hu <wei.hu@enflame-tech.com>
2021-10-11 23:00:40 -0400

    switch autodiff to reverse mode

--- src/Cone/AutoDiff.hs
+++ src/Cone/AutoDiff.hs
@@ -47,18 +47,16 @@ setupDiff d f@FuncDef{..} = do
   argTypes <- mapM (\a -> do
     let i = L.findIndex (\(e, _) -> a == e) _funcArgs
     case i of
-      Just i -> return (_funcArgs !! i ^. _1 ++ "____diff", _funcArgs !! i ^. _2)
+      Just i -> return $ _funcArgs !! i ^. _2
       Nothing -> throwError $ "cannot find wrt " ++ a ++ " in " ++ ppr f ++ ppr _funcLoc
     ) (_diffWRT d)
-  let argT:resT = reverse $ argTypes ^.. traverse  . _2
+  let argT:resT = reverse argTypes
       resTypes = L.foldl' (\t e -> TApp (TVar (s2n "core/prelude/pair") (_tloc e)) [t, e] (_tloc e)) argT resT
       fType = bindTypeEffVar _funcBoundEffVars $ bindTypeVar _funcBoundVars $
          TFunc (_funcArgs ^.. traverse . _2 ++ [_funcResultType]) (EffList [] _funcLoc) resTypes _funcLoc
-  -- id <- fresh
-  let fn = _funcName ++ "____diff" -- ++ show id
+  let fn = _funcName ++ "____diff"
   setFuncType fn fType
-  -- setEnv (Just _funcName) $ diffMapping . at fn
-  return f{_funcName = fn, _funcArgs = _funcArgs ++ argTypes}
+  return f{_funcName = fn, _funcArgs = _funcArgs ++ [("____output____diff", _funcResultType)]}
 setupDiff d BoundFuncDef{..} = do
   let (_, f) = unsafeUnbind _boundFuncDef
   setupDiff d f
@@ -75,7 +73,7 @@ genDiffs m = do
       Just d ->
         if isn't _Just $ _diffAdj d
         then do
-          (\f -> (d ^. diffFunc, f):ds) <$> (setupDiff d f >>= genDiff)
+          (\f -> (d ^. diffFunc, f):ds) <$> (setupDiff d f >>= addTempVariables >>= genDiff d)
         else return ds
       Nothing -> return ds)
     []
@@ -107,78 +105,113 @@ genConstantByType c t@(TPrim pt _) = do
   return $ ELit c' t (_tloc t)
 genConstantByType _ t = throwError $ "unsupported type " ++ ppr t ++ ppr (_tloc t)
 
-genDiffForExpr :: (Has EnvEff sig m) => Expr -> m Expr
-genDiffForExpr e@EVar{..} = return e{_evarName=s2n (name2String _evarName ++ "____diff")}
-genDiffForExpr a@(EApp False (EVar n _) targs args loc) = do
+addTempVariables :: (Has EnvEff sig m) => FuncDef -> m FuncDef
+addTempVariables = transformMOn (funcExpr . _Just) addTempVar
+  where addTempVar a@(EApp False _ targs args loc) = do
+          tempVs <- mapM (\arg -> fresh >>= (\id -> return $ "____temp" ++ show id)) args
+          let newApp = a{_eappArgs=map (\v -> EVar (s2n v) loc) tempVs}
+          (_, e) <- foldM (\(i, newApp) vn -> do
+            return (i - 1, ELet (PVar (s2n vn) loc) (args !! i) newApp False loc))
+            (L.length args - 1, newApp)
+            (reverse tempVs)
+          return e
+        addTempVar e = return e
+
+genDiffForExpr :: (Has EnvEff sig m) => String -> Expr -> m (Expr, [(String, Type)], [(Pattern, Expr, Bool)])
+genDiffForExpr outputD e@EVar{..} = do
+  let diffN = name2String _evarName ++ "____diff"
+      diff = EVar (s2n diffN) _eloc
+  return (EApp False (EVar (s2n "core/prelude/____assign") _eloc) []
+          [diff, EApp False (EVar (s2n "core/prelude/____add") _eloc) [] 
+          [diff, EVar (s2n (outputD ++ "____diff")) _eloc] _eloc] _eloc, [(diffN, TPrim F32 _eloc)], [])
+genDiffForExpr outputD a@(EApp False (EVar n _) targs args loc)= do
   let fn = name2String n
+      od = EVar (s2n (outputD ++ "____diff")) loc
   f <- getEnv $ diffAdjs . at fn
   forMOf _Nothing f $ \_ ->
     throwError $ "cannot find diff adj for function " ++ ppr fn ++ ppr loc
-  diffs <- mapM genDiffForExpr args
-  return $ EApp False (fromJust $ _diffAdj $ fromJust f) targs (args ++ diffs) loc
-genDiffForExpr s@ESeq{..} = do
-  es <- mapM genDiffForExpr _eseq
-  return s{_eseq=es}
-genDiffForExpr l@(ELet (PVar v _) e body s loc) = do
-  let vn = name2String v
-      dvn = vn ++ "____diff"
-  de <- genDiffForExpr e
-  db <- genDiffForExpr body
-  let newBody = ELet (PVar (s2n dvn) loc) de db s loc
-  return l{_eletBody=newBody}
-genDiffForExpr w@EWhile{..} = do
-  db <- genDiffForExpr _ewhileBody
-  return w{_ewhileBody=db}
-genDiffForExpr c@(ECase _
-    [t@(Case (PExpr (ELit "true"  (TPrim Pred _) _) _) _ te _)
-    ,f@(Case (PExpr (ELit "false" (TPrim Pred _) _) _) _ fe _)] loc) = do
-  dte <- genDiffForExpr te
-  dfe <- genDiffForExpr fe
-  return c{_ecaseBody=[t{_caseExpr=dte}, f{_caseExpr=dfe}]}
-genDiffForExpr l@ELit{..} = genConstantByType "0" _litType
-genDiffForExpr a@EAnn{..} = do
-  d <- genDiffForExpr _eannExpr
-  return a{_eannExpr=d}
-genDiffForExpr a@EAnnMeta{..} = do
-  d <- genDiffForExpr _eannMetaExpr
-  return a{_eannMetaExpr=d}
-genDiffForExpr b@EBoundTypeVars{..} = do
+  fdef <- getEnv $ funcDefs . at fn
+  forMOf _Nothing f $ \_ ->
+    throwError $ "cannot find finction " ++ ppr fn ++ ppr loc
+  inputDs <- L.nub <$> foldM (\all (arg, farg) -> case arg of
+                            EVar an _ -> if farg ^._1 `elem` _diffWRT (fromJust f) then return $ all ++ [(name2String an ++ "____diff", farg ^._2)] else return all
+                            e -> throwError $ "unsupported " ++ ppr e) [] [(arg, farg) | arg <- args | farg <- fromJust fdef ^. funcArgs]
+  diffs <- mapM (\_ -> fresh >>= (\id -> return $ "____temp" ++ show id)) inputDs
+  let d:ds = reverse diffs
+      p = L.foldl' (\s e -> PApp (EVar (s2n "core/prelude/pair") loc) [] [PVar (s2n e) loc, s] loc) (PVar (s2n d) loc) ds
+      setDiffs = [EApp False (EVar (s2n "core/prelude/____assign") loc) []
+                     [EVar (s2n n) loc, EApp False (EVar (s2n "core/prelude/____add") loc) [] [EVar (s2n n) loc, EVar (s2n t) loc] loc] loc | n <- inputDs ^.. traverse._1 | t <- diffs]
+  return (ELet p (EApp False (fromJust $ _diffAdj $ fromJust f) targs (args ++ [od]) loc) (ESeq setDiffs loc) False loc, inputDs, [])
+genDiffForExpr outputD s@ESeq{..} = do
+  es <- mapM (genDiffForExpr outputD) (reverse _eseq)
+  return (s{_eseq=es ^.. traverse ._1}, join $ es ^.. traverse ._2, join $ es ^.. traverse . _3)
+genDiffForExpr outputD l@(ELet p@(PVar v _) e body False loc) = do
+  (db, bvs, bps) <- genDiffForExpr outputD body
+  (de, evs, eps) <- genDiffForExpr (name2String v) e
+  return (ESeq [db, de] loc, bvs ++ evs, (p, e, False):bps ++ eps)
+-- genDiffForExpr outputD w@EWhile{..} = do
+--   db <- genDiffForExpr outputD _ewhileBody
+--   return (w{_ewhileBody=db ^. _1}, db ^. _2, db ^. _3)
+-- genDiffForExpr outputD c@(ECase _
+--     [t@(Case (PExpr (ELit "true"  (TPrim Pred _) _) _) _ te _)
+--     ,f@(Case (PExpr (ELit "false" (TPrim Pred _) _) _) _ fe _)] loc) = do
+--   dte <- genDiffForExpr outputD te
+--   dfe <- genDiffForExpr outputD fe
+--   return (c{_ecaseBody=[t{_caseExpr=dte ^._1}, f{_caseExpr=dfe ^._1}]}, dte ^._2 ++ dfe ^._2, dte ^. _3 ++ dfe ^. _3)
+genDiffForExpr outputD l@ELit{..} = (, [], []) <$> genConstantByType "0" _litType
+genDiffForExpr outputD a@EAnn{..} = do
+  d <- genDiffForExpr outputD _eannExpr
+  return (a{_eannExpr=d ^. _1}, d ^. _2, d ^. _3)
+genDiffForExpr outputD a@EAnnMeta{..} = do
+  d <- genDiffForExpr outputD _eannMetaExpr
+  return (a{_eannMetaExpr=d ^. _1}, d ^. _2, d ^. _3)
+genDiffForExpr outputD b@EBoundTypeVars{..} = do
   let (vs, e) = unsafeUnbind _eboundTypeVars
-  d <- genDiffForExpr e
-  return b{_eboundTypeVars=bind vs d}
-genDiffForExpr b@EBoundEffTypeVars{..} = do
+  d <- genDiffForExpr outputD e
+  return (b{_eboundTypeVars=bind vs $ d ^. _1}, d ^._2, d ^. _3)
+genDiffForExpr outputD b@EBoundEffTypeVars{..} = do
   let (vs, e) = unsafeUnbind _eboundEffTypeVars
-  d <- genDiffForExpr e
-  return b{_eboundEffTypeVars=bind vs d}
-genDiffForExpr b@EBoundVars{..} = do
+  d <- genDiffForExpr outputD e
+  return (b{_eboundEffTypeVars=bind vs $ d ^._1}, d ^. _2, d ^. _3)
+genDiffForExpr outputD b@EBoundVars{..} = do
   let (vs, e) = unsafeUnbind _eboundVars
-  d <- genDiffForExpr e
-  return b{_eboundVars=bind vs d}
-genDiffForExpr e = throwError $ "unsupported expr for diff " ++ ppr e ++ ppr (_eloc e)
+  d <- genDiffForExpr outputD e
+  return (b{_eboundVars=bind vs $ d ^. _1}, d ^._2, d ^. _3)
+genDiffForExpr outputD e = throwError $ "unsupported expr for diff " ++ ppr e ++ ppr (_eloc e)
 
-genDiff :: (Has EnvEff sig m) => FuncDef -> m FuncDef
-genDiff f@FuncDef{} = do
+genDiff :: (Has EnvEff sig m) => DiffDef -> FuncDef -> m FuncDef
+genDiff diff f@FuncDef{} = do
   let loc = _funcLoc f
-  e <- mapM genDiffForExpr (_funcExpr f)
-  return f{_funcExpr = e}
-genDiff BoundFuncDef{..} = do
+  case _funcExpr f of 
+    Just e -> do
+                (e, vs, ps) <- genDiffForExpr "____output" e
+                let d:ds = _diffWRT diff
+                    diffs = L.foldl' (\s e -> EApp False (EVar (s2n "core/prelude/pair") loc) [] [EVar (s2n e) loc, s] loc) (EVar (s2n d) loc) ds
+                e <- foldM (\s (e,t) -> do
+                  c0 <- genConstantByType "0" (TPrim F32 loc)
+                  return $ ELet (PVar (s2n e) loc) c0 s True loc) (ESeq [e, diffs] loc) (L.nubBy (\a b -> fst a == fst b) vs)
+                e <- foldM (\s (p, e, iss) -> do
+                  return $ ELet p e s iss loc) e (reverse ps)
+                return f{_funcExpr = Just e}
+    Nothing -> return f
+genDiff d BoundFuncDef{..} = do
   let (vs, f) = unsafeUnbind _boundFuncDef
-  f' <- genDiff f
+  f' <- genDiff d f
   return $ BoundFuncDef (bind vs f') _funcLoc
-genDiff BoundEffFuncDef {..} = do
+genDiff d BoundEffFuncDef {..} = do
   let (vs, f) = unsafeUnbind _boundEffFuncDef
-  f' <- genDiff f
+  f' <- genDiff d f
   return $ BoundEffFuncDef (bind vs f') _funcLoc
 
 replaceDiffFuncCalls :: (Has EnvEff sig m) => Module -> m Module
-replaceDiffFuncCalls m = 
+replaceDiffFuncCalls m =
   mapMOf (topStmts . traverse . _FDef . funcExpr . _Just) replaceDiffFuncCall m
   >>= mapMOf (topStmts. traverse. _ImplFDef . implFunDef . funcExpr . _Just) replaceDiffFuncCall
   where replaceDiffFuncCall expr = transformM replace expr
         replace e@EApp{..} = do
           if _eappDiff then do
             let f = _eappFunc
-            when (isn't _EVar f) $ 
+            when (isn't _EVar f) $
                throwError $ "expected a function name, but got " ++ ppr _eappFunc ++ ppr _eloc
             let n = name2String $ _evarName f
             d <- getEnv $ diffAdjs . at n
